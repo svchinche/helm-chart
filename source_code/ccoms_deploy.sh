285c152bfbfa85451d36329a2051b6970520ca14
schinche <suyog_chinche_tp@bmc.com>
2020-06-15 22:41:18 +0530

    updated script

--- source_code/ccoms_deploy.sh
+++ source_code/ccoms_deploy.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-helm install --debug --dry-run  ccoms --create-namespace --namespace=mongo --dependency-update  --set mongo.persistence.networkResource=nfs --set mongo.persistence.server=clm-pun-ua9914 --set  global.externalIPaddress=10.135.35.201 mongo
+## delete role if it is present
+kubectl delete clusterrolebindings.rbac.authorization.k8s.io --field-selector metadata.name=mongo-view
+
+## install ccoms project
+helm install --debug ccoms --create-namespace --namespace=mongo  --generate-name --dependency-update --set mongo.persistence.networkResource=nfs --set mongo.persistence.server=clm-pun-ua9914 --set  global.externalIPaddress=10.135.35.201 
+
