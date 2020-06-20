718ddb1bd096921047a8f120a81f9c259f3a67a8
schinche <suyog_chinche_tp@bmc.com>
2020-06-20 11:08:35 +0530

    Updated file

--- source_code/create_charts.sh
+++ source_code/create_charts.sh
@@ -3,6 +3,9 @@
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
+## Delete all charts before creating it
+rm -rf $SCRIPTPATH/../*.tgz
+
 projects=(ccoms common config department employee mongo organization proxy)
 
 for project in ${projects[@]}
